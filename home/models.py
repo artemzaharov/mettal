91b78bed5c4f8df68777db39a37cfb18b2a6f198
Your Name <you@example.com>
2023-03-18 21:44:49 +0100

    coomit headers

--- home/models.py
+++ home/models.py
@@ -3,26 +3,37 @@ from wagtail.fields import StreamField
 from wagtail.models import Page
 from wagtail.snippets.models import register_snippet
 from streams import blocks
-from wagtail.admin.edit_handlers import FieldPanel
-
+from wagtail.admin.panels import FieldPanel
 
 
 @register_snippet
 class Menu(models.Model):
-    ''' Alows to translate menus '''
     menu = StreamField([        
         ('menus', blocks.MenuBlock())
-    ])
+    ], use_json_field=True)
 
     def __str__(self):
         return 'Menu'
 
 
+@register_snippet
+class Header(models.Model):
+    header = StreamField([        
+        ('header', blocks.HeaderBlock())
+    ], use_json_field=True)
+
+    def __str__(self):
+        return 'Header'
+
 class HomePage(Page):
+    header = models.ForeignKey(
+        'Header', on_delete=models.SET_NULL, null=True, blank=True, related_name='+'
+    )
     menu = models.ForeignKey(
         'Menu', on_delete=models.SET_NULL, null=True, blank=True, related_name='+'
     )
 
     content_panels = Page.content_panels + [
+        FieldPanel("header"),
         FieldPanel("menu"),
     ]
--- streams/blocks.py
+++ streams/blocks.py
@@ -3,10 +3,27 @@ from wagtail.images.blocks import ImageChooserBlock
 
 class MenuBlock(blocks.StructBlock):
 
-    title_menu = blocks.CharBlock(required=True, help_text='Add your title')
-    title_url = blocks.URLBlock(required=True, help_text='Add additional text')
+    title_menu = blocks.CharBlock(required=True, help_text='Название меню')
+    title_url = blocks.URLBlock(required=True, help_text='Ссылка на страницу')
     
     class Meta:  # noqa
         template = 'streams/menu_block.html'
         icon = "edit"
-        label = "Menus"
+        label = "Меню"
+
+class HeaderBlock(blocks.StructBlock):
+
+    logo = ImageChooserBlock(required=False, help_text='Добавить логотип')
+    call_us_text = blocks.CharBlock(required=True, help_text='Текст с призывом позвонить')
+    call_us_number = blocks.CharBlock(required=True, help_text='Номер телефона')
+    email_text = blocks.CharBlock(required=True, help_text='Текст c призывом написать email')
+    email_address = blocks.CharBlock(required=True, help_text='Email адрес')
+    address_text = blocks.CharBlock(required=True, help_text='Текст с призывом приехать')
+    address = blocks.CharBlock(required=True, help_text='Адрес')
+    apply_button_text = blocks.CharBlock(required=True, help_text='Кнопка заявки')
+    apply_button_url = blocks.URLBlock(required=True, help_text='Ссылка кнопки')
+
+    class Meta:  # noqa
+        template = 'streams/header_block.html'
+        icon = "edit"
+        label = "Верхний блок" 
