9eb7ca5e95c845f40401b040a9aeed6a0ee866c1
Александр Родионов <rexarrior@yandex.ru>
2023-03-22 21:31:30 +0300

    fix csrf for docker deployment

--- metall/settings/dev.py
+++ metall/settings/dev.py
@@ -8,7 +8,8 @@ SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7
 
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', "server"]
-CSRF_TRUSTED_ORIGINS = ["https://a81d-83-50-248-74.ngrok.io/",]
+# CSRF_TRUSTED_ORIGINS = ["https://a81d-83-50-248-74.ngrok.io/",]
+CSRF_TRUSTED_ORIGINS = ["http://localhost",]
 
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
