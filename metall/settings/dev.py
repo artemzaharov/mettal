0fb8d2f2de1f138c994ac10d3848a382e43ba927
Your Name <you@example.com>
2023-04-02 18:18:12 +0200

    commit dev.py

--- metall/settings/dev.py
+++ metall/settings/dev.py
@@ -1,20 +1,20 @@
-from .base import *
+# from .base import *
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# # SECURITY WARNING: don't run with debug turned on in production!
+# DEBUG = True
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
+# # SECURITY WARNING: keep the secret key used in production secret!
+# SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
 
-# SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', "server", "http://spbvtormetlom.ru"]
-# CSRF_TRUSTED_ORIGINS = ["https://a81d-83-50-248-74.ngrok.io/",]
-CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://spbvtormetlom.ru"]
+# # SECURITY WARNING: define the correct hosts in production!
+# ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', "server", "http://spbvtormetlom.ru"]
+# # CSRF_TRUSTED_ORIGINS = ["https://a81d-83-50-248-74.ngrok.io/",]
+# CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://spbvtormetlom.ru"]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 
 
-try:
-    from .local import *
-except ImportError:
-    pass
+# try:
+#     from .local import *
+# except ImportError:
+#     pass
--- metall/settings/production.py
+++ metall/settings/production.py
@@ -7,5 +7,7 @@ try:
 except ImportError:
     pass
 
+SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
+
 CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://spbvtormetlom.ru"]
 ALLOWED_HOSTS = ["server", "http://spbvtormetlom.ru"]
