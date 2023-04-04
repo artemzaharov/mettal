775e40e1ddaba4e9b87945e3462a7df6d1996e51
Your Name <you@example.com>
2023-04-04 20:32:38 +0200

    add https to allowed_hosts

--- metall/settings/production.py
+++ metall/settings/production.py
@@ -10,4 +10,4 @@ except ImportError:
 SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://spbvtormetlom.ru", "https://www.spbvtormetlom.ru"]
-ALLOWED_HOSTS = ["localhost","server", "http://spbvtormetlom.ru"]
+ALLOWED_HOSTS = ["localhost","server", "http://spbvtormetlom.ru", "https://www.spbvtormetlom.ru"]
