2b160cb097fa0ff1b4335782f0ebd5cbe5f040a2
Your Name <you@example.com>
2023-04-07 23:19:28 +0200

    sitemap

--- metall/settings/base.py
+++ metall/settings/base.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = [
     "streams",
     "wagtail.contrib.forms",
     "wagtail.contrib.redirects",
+    "wagtail.contrib.sitemaps",
     "wagtail.embeds",
     "wagtail.sites",
     "wagtail.users",
@@ -47,6 +48,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sitemaps",
 ]
 
 MIDDLEWARE = [
