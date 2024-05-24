0503093959a89f83af3a61c5d89c0cb7ea33889b
Artem Zakharov <58831797+artemzaharov@users.noreply.github.com>
2024-05-24 23:27:57 +0200

    Update dev.py

--- metall/settings/dev.py
+++ metall/settings/dev.py
@@ -4,7 +4,7 @@
 # DEBUG = True
 
 # # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
+# SECRET_KEY = "fake key"
 
 # # SECURITY WARNING: define the correct hosts in production!
 # ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.ngrok.io', "server", "http://spbvtormetlom.ru"]
