ddff16cd9b082ea664e355757bf2556f6a36a631
Artem Zakharov <58831797+artemzaharov@users.noreply.github.com>
2024-05-24 23:27:24 +0200

    Update production.py

--- metall/settings/production.py
+++ metall/settings/production.py
@@ -7,7 +7,7 @@ try:
 except ImportError:
     pass
 
-SECRET_KEY = "django-insecure-67-r&^f89dg!6%vk3o%qu7vf1o9$t5yzwy_x%vq$zw+2&oe4m7"
+SECRET_KEY = "Fake key"
 
 CSRF_TRUSTED_ORIGINS = ["http://localhost", "http://spbvtormetlom.ru", "https://www.spbvtormetlom.ru", "https://spbvtormetlom.ru", ]
 ALLOWED_HOSTS = ["localhost","server", "http://spbvtormetlom.ru", "https://www.spbvtormetlom.ru"]
